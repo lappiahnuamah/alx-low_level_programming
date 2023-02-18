18463bac238c127f3d1760112f8c5a25682deea4
Lawrence Appiah-Nuamah <lappiahnuamahfbc1999@gmail.com>
2023-02-18 09:17:01 -0800

    Prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
-
 /**
- * main - prints all numbers in bae 16 in lowercase
- * followed by a new line
+ * main - is entry of program
  *
- * Return: Always 0
+ * Return: 0, if succesful
  */
 int main(void)
 {
-	int n, m;
+	/*
+	 * prints all combination of 2 different digits once,in accending order
+	 * after each combination follows a coma and a space
+	 */
 
-	for (n = 48; n < 56; n++)
+	/* take ab as possible digits */
+	int a, b;
+
+	/*loop through a,1st digit,lowest of the 2, highest possible is 8*/
+	for (a = 0 ; a <= 8 ; a++)
 	{
-		for (m = 49; m <= 57; m++)
+		/*loop through b,2nd digit,higher than a by 1,highest possible is 9*/
+		for (b = a + 1 ; b <= 9 ; b++)
 		{
-			if (m > n)
-			{
-				putchar(n);
-				putchar(m);
-				if (n != 56 || m != 57)
-				{
-					putchar(',');
-					putchar('');
-			}
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+			/*continues to put coma,space,stop if last highest combination is met*/
+			if (a == 8 && b == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
