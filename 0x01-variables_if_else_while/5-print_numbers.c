c523abe956b667d46f7a0f963c654df789d3cf01
Lawrence Appiah-Nuamah <lappiahnuamahfbc1999@gmail.com>
2023-02-17 21:18:06 -0800

    prints al single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase
+ * followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d\n", n);
+	return (0);
+}
