1386c1eec84ec7604aa7240f200680786379f117
Lawrence Appiah-Nuamah <lappiahnuamahfbc1999@gmail.com>
2023-02-18 08:50:21 -0800

    Prints all single digit numbers of bse 10 starting from 0

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,15 +2,13 @@
 #include <ctype.h>
 /**
  * main - prints all single digit numbers of
- * starting from
- * followed by a new line
- *
+ * starting from followed by a new line
  * Return: Always 0
  */
 int main(void)
 {
 	int n;
-	
+
 	for (n = 0; n < 10; n++)
 		printf("%d", n);
 	printf("\n");
