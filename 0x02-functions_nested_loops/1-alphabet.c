d076b864904abd4780f995e53dce3249b2a5df3d
Lawrence Appiah-Nuamah <lappiahnuamahfbc1999@gmail.com>
2023-02-21 20:50:19 -0800

    Prints the alphabet, in lowercase followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,16 @@
-#include <main.h>
+#include "main.h"
 /**
- * main - Entry point
- * prints the alphabet, in lowercase
+ * print_alphabet -  prints the alphabet, in lowercase
  *
  * Return - Always 0 (Success)
  */
+void print_alphabet(void)
+{
+	char c;
 
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
