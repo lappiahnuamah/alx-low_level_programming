8801d581244a32aedbfd229bb133b3db26382249
Lawrence Appiah-Nuamah <lappiahnuamahfbc1999@gmail.com>
2023-02-21 21:03:27 -0800

    Prints 10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,11 +4,12 @@
  *
  * Return - Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
+	int i;
 	char c;
-	
-	for (int i=0; i<10; i++)
+
+	for (i = 0; i < 10; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
